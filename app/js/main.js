0366d25a6a737d0535a7135f1f5a0e453d764be2
Weadmead <wead758@gmail.com>
2022-11-26 00:40:15 +0300

    added sections slider + subscribe + footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
 $(function () {
 
     $('.slider__inner').slick({
-
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '120px',
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.png" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.png" alt="arrow-next"></button>',
     });
 
 });
